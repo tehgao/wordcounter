45ab7312fea2783ae4dafd357cfa71a6c0a49168
Alvin Gao <gaovinal@gmail.com>
2019-10-10 01:05:13 -0400

    Added test for getting a sentence that contains a word

--- src/test/java/com/alvingao/wordcounter/FrequencyAnalyzerTest.java
+++ src/test/java/com/alvingao/wordcounter/FrequencyAnalyzerTest.java
@@ -11,11 +11,13 @@ import java.util.Map;
 import com.alvingao.wordcounter.analyzers.FrequencyAnalyzer;
 import com.alvingao.wordcounter.analyzers.Tokenizer;
 import com.alvingao.wordcounter.exceptions.TokenizationException;
+import com.alvingao.wordcounter.sentencestructure.Punctuation;
+import com.alvingao.wordcounter.sentencestructure.Word;
 
 import org.junit.Test;
 
 public class FrequencyAnalyzerTest {
-    public final String mobyDick = "Call me Ishmael. Some years ago, never mind how long precisely, having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off, then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me.";
+    private final String mobyDick = "Call me Ishmael. Some years ago, never mind how long precisely, having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off, then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me.";
 
     @Test
     public void testCreateFrequencyTable() throws TokenizationException {
@@ -61,6 +63,18 @@ public class FrequencyAnalyzerTest {
 
         List<String> actual = classUnderTest.getTopWords(3);
         List<String> expected = new ArrayList<>(Arrays.asList("the", "i", "and"));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testGetSentencesWithWordSingleton() throws TokenizationException {
+        List<Sentence> corpus = (new Tokenizer()).corpusToSentences(this.mobyDick);
+        FrequencyAnalyzer classUnderTest = new FrequencyAnalyzer(corpus);
+
+        List<Sentence> actual = classUnderTest.getSentencesWithWord("surprising");
+        List<Sentence> expected = new ArrayList<>(Arrays.asList(new Sentence(new Word("There"), new Word("is"),
+                new Word("nothing"), new Word("surprising"), new Word("in"), new Word("this"), new Punctuation("."))));
 
         assertEquals(expected, actual);
     }
